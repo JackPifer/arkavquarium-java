67956dd13b6892d1dca2afd053348d21aea39f26
Steven Sukma <steven.sukma.limanus@gmail.com>
2018-04-24 19:44:42 +0700

    ADD javadocs

--- src/Aquarium.java
+++ src/Aquarium.java
@@ -1,7 +1,16 @@
 import java.awt.*;
 
+/** Represents an aquarium
+ * @version 1.0
+*/
 public class Aquarium implements Drawable{
+    /** 
+     * Max Width of the app window 
+     */
     public static final int DEFAULT_WIDTH = 650;
+    /** 
+     * Max Height of the app window 
+     */
     public static final int DEFAULT_HEIGHT = 508;
 
     private LinkedList<Piranha> listOfPiranha;
@@ -10,6 +19,9 @@ public class Aquarium implements Drawable{
     private LinkedList<FishFood> listOfFishFood;
     private Snail snail;
 
+    /** 
+     * Aquarium Constructor
+     */
     public Aquarium() {
         snail = new Snail();
         listOfPiranha = new LinkedList<>();
@@ -18,58 +30,116 @@ public class Aquarium implements Drawable{
         listOfFishFood = new LinkedList<>();
     }
 
+    /** 
+     * Snail prop getter
+     * @return object of class Snail
+     */
     public Snail getSnail() {
         return this.snail;
     }
 
+    /** 
+     * listOfPiranha prop getter
+     * @return object of class LinkedList of Piranha
+     */
     public LinkedList<Piranha> getListOfPiranha() {
         return this.listOfPiranha;
     }
 
+    /** 
+     * listOfGuppy prop getter
+     * @return object of class LinkedList of Guppy
+     */
     public LinkedList<Guppy> getListOfGuppy() {
         return this.listOfGuppy;
     }
 
+    /** 
+     * listOfFishFood prop getter
+     * @return object of class LinkedList of FishFood
+     */
     public LinkedList<FishFood> getListOfFishFood() {
         return this.listOfFishFood;
     }
 
+    /** 
+     * listOfCoin prop getter
+     * @return object of class LinkedList of Coin
+     */
     public LinkedList<Coin> getListOfCoin() {
         return this.listOfCoin;
     }
 
+    /** 
+     * add guppy to listOfguppy
+     * @param guppy Guppy object to add
+     */
     public void addGuppy(Guppy guppy) {
         this.listOfGuppy.add(guppy);
     }
 
+    /** 
+     * add piranha to listOfPiranha
+     * @param piranha Piranha object to add
+     */
     public void addPiranha(Piranha piranha) {
         this.listOfPiranha.add(piranha);
     }
 
+    /** 
+     * add coin to listOfCoin
+     * @param coin Coin object to add
+     */
     public void addCoin(Coin coin) {
         this.listOfCoin.add(coin);
     }
 
+    /** 
+     * add food to listOfFishFood
+     * @param food FishFood object to add
+     */
     public void addFishFood(FishFood food) {
         this.listOfFishFood.add(food);
     }
     
+    /** 
+     * remove guppy from listOfguppy
+     * @param guppy Guppy object to remove
+     */
     public void removeGuppy(Guppy guppy) {
         this.listOfGuppy.remove(guppy);
     }
 
+    /** 
+     * remove piranha from listOfPiranha
+     * @param piranha Piranha object to remove
+     */
     public void removePiranha(Piranha piranha) {
         this.listOfPiranha.remove(piranha);
     }
 
+    /** 
+     * remove coin from listOfCoin
+     * @param coin Coin object to remove
+     */
     public void removeCoin(Coin coin) {
         this.listOfCoin.remove(coin);
     }
 
+    /** 
+     * remove food from listOfFishFood
+     * @param food FishFood object to remove
+     */
     public void removeFishFood(FishFood food) {
         this.listOfFishFood.remove(food);
     }
 
+    /** 
+     * draw to aquarium
+     * @param g Draw container
+     * @param t Object to grab image
+     * @param con Game controller
+     */
     public void draw(Graphics g, Toolkit t, Controller con) {
         g.drawImage(t.getImage("images/Aquarium4.jpg"), 0, 0, con);
 
--- src/Coin.java
+++ src/Coin.java
@@ -2,26 +2,48 @@
 // Responsibility : Act as Coin for player to get to increase money
 
 import java.awt.*;
-
+/** 
+ * Representas a coin
+ * @version 1.0
+ */
 public class Coin extends DroppableItem implements Drawable{
     private double value;
 
     // User-defined Constructor
+    /** 
+     * constructor
+     * @param value value of coin
+     * @param fishPosition Position of fish
+     */
     public Coin(double value, Position fishPosition){
         super(fishPosition);
         this.value = value;
     }
 
     //Getter
+    /** 
+     * value getter
+     * @return value
+     */
     public double getValue() {
         return this.value;
     }
 
     //Setter
+    /** 
+     * value setter
+     * @param value value to be set
+     */
     public void setValue(double value) {
         this.value = value;
     }
 
+    /** 
+     * draw to aquarium
+     * @param g Draw container
+     * @param t Object to grab image
+     * @param con Game controller
+     */
     public void draw(Graphics g, Toolkit t, Controller con) {
         if(this.getValue()==25) {
             g.drawImage(t.getImage("images/Silver_coin.png"), (int) getCurrentPosition().getX(), (int) getCurrentPosition().getY(), con);
