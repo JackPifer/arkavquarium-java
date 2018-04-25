6ae9c154ae0f75e79e5125a2c9a86dd17d5b439a
Steven Sukma <steven.sukma.limanus@gmail.com>
2018-04-25 11:21:10 +0700

    ADD javadoc Node

--- src/Node.java
+++ src/Node.java
@@ -2,19 +2,34 @@ class Node<T> {
   private T value;
   private Node<T> next;
 
+  /** 
+   * constructor.
+   * @param value value of node
+   */
   public Node(T value) {
     this.value = value;
     this.next = null;
   }
-
+  /** 
+   * return next element (next node)
+   * @return Object of Node
+   */
   public Node<T> getNext() {
     return next;
   }
 
+    /** 
+   * getter.
+   * @return value of current node.
+   */
   public T getValue() {
     return value;
   }
 
+    /** 
+   * setter.
+   * @param next set next property.
+   */
   public void setNext(Node<T> next) {
     this.next = next;
   }
